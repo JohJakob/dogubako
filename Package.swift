3acdf3eb6f1d1c5d61c25e05fa3bc8ec2590115d
Johannes Jakob <hi@johjakob.com>
2025-07-07 14:57:35 +0200

    Package: Add package description

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import CompilerPluginSupport
+
+let package = Package(
+    name: "dogubako",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+        .watchOS(.v4),
+        .tvOS(.v12),
+        .visionOS(.v1)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "dogubako",
+            targets: ["dogubako"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "dogubako"),
+        .testTarget(
+            name: "dogubakoTests",
+            dependencies: ["dogubako"]
+        ),
+    ]
+)
